9350ea059bb026cb2f7058890f07872b9394e617
Javier Lurgo (ntbk) <jlurgo@gmail.com>
2015-06-11 00:05:54 -0300

    preparando para phonegap

--- adaptador_usb.js
+++ adaptador_usb.js
@@ -32,7 +32,7 @@ $(function(){
 
 				setInterval(function(){
 
-					read_callback((Math.random() * 100.0).toFixed(3));
+					read_callback(((Math.random() * 200.0) - 100).toFixed(3)+" mm\n");
 
 				}, 3000);
 			},
@@ -54,8 +54,8 @@ $(function(){
 	 * FIN de Mock de objeto serial
 	 **************************************************/
 		
+	var buffer_entrada_serie = "";
 	var abrirPuertoSerie = function(){
-		
 		serial.open (
 			{baudRate: 115200},
 			
@@ -82,20 +82,23 @@ $(function(){
 							var dataArray = new Uint8Array(data);
 							var dataString = String.fromCharCode.apply(null, dataArray);
 						}
-						// Mensaje definido en:
-						// https://docs.google.com/document/d/1y0_301NuTZICPXeorPcrvl9MDEuBTutYIUK0CI5smXY/edit
-						// TODO:
-						// adaptar con dataFormated real
 						
-						console.log("llego de instrumento:", dataString);
-						var mensaje = {
-							tipoDeMensaje:"medicion",
-							//valor: String.fromCharCode.apply(null, dataFormated),
-							valor: parseFloat(dataString).toString(),
-							unidad:"cm"
+						buffer_entrada_serie += dataString;
+						var mensajes_en_buffer = buffer_entrada_serie.split('\n');
+						for(var i=0; i<mensajes_en_buffer.length-1; i++){
+							console.log("llego de instrumento:", mensajes_en_buffer[i]);
+							// Mensaje definido en:
+							// https://docs.google.com/document/d/1y0_301NuTZICPXeorPcrvl9MDEuBTutYIUK0CI5smXY/edit
+							// TODO:
+							// adaptar con dataFormated real
+							var mensaje = {
+								tipoDeMensaje:"medicion",
+								valor: mensajes_en_buffer[i].split(' ')[0],
+								unidad: mensajes_en_buffer[i].split(' ')[1]
+							}							
+							Vx.send(mensaje);
 						}
-						
-						Vx.send(mensaje);
+						buffer_entrada_serie = mensajes_en_buffer[mensajes_en_buffer.length-1];
 						
 					},
 					function(err){
